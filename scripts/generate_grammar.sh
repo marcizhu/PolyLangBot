239764b15cf719433e2585daedd9cdb06875d3be
marcizhu <marc.izquierdo.hu@gmail.com>
2021-01-03 12:31:11 +0100

    Cleanup, added more examples, organized files and other minor changes

--- scripts/generate_grammar.sh
+++ scripts/generate_grammar.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cd cl
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd "$DIR/../cl"
 java -jar antlr-4.7.1-complete.jar -Dlanguage=Python3 -no-listener -visitor PolyLang.g
