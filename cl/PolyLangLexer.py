abafb590d641f3f0e141b020eb18aa851ddea798
marcizhu <marc.izquierdo.hu@gmail.com>
2021-01-10 12:10:06 +0100

    Upgrade ANTLR from v4.7.1 to v4.9.1

--- cl/PolyLangLexer.py
+++ cl/PolyLangLexer.py
@@ -1,8 +1,9 @@
-# Generated from ../cl/PolyLang.g by ANTLR 4.7.1
+# Generated from ../cl/PolyLang.g by ANTLR 4.9.1
 from antlr4 import *
 from io import StringIO
 from typing.io import TextIO
 import sys
+
 
 
 def serializedATN():
@@ -194,7 +195,7 @@ class PolyLangLexer(Lexer):
 
     def __init__(self, input=None, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.7.1")
+        self.checkVersion("4.9.1")
         self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
         self._actions = None
         self._predicates = None
