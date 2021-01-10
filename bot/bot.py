8cbcd817347e86277fcbc4b5123c13690f3718be
marcizhu <marc.izquierdo.hu@gmail.com>
2021-01-10 01:15:24 +0100

    Save images to memory, return output using Markdown

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,7 @@
+from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+import io  # For io.BytesIO() (aka saving data to memory)
 from antlr4 import *
 from cl.TreeVisitor import TreeVisitor  # Tree Visitor for this language
 from cl.PolyLangLexer import PolyLangLexer
@@ -11,16 +13,19 @@ def run():
         """Main message handler. Recieves a message, executes its contents and returns the output"""
 
         def execute(text, visitor):
-            """Executes `text` and returns the output. Uses `visitor` to visit the AST"""
+            """Executes `text` and returns the output in Markdown format. Uses `visitor` to visit the AST"""
             try:
                 lexer = PolyLangLexer(InputStream(text))
                 token_stream = CommonTokenStream(lexer)
                 parser = PolyLangParser(token_stream)
                 tree = parser.prog()
                 visitor.visit(tree)
-                return visitor.output
+                # Return output inside a code block using Markdown
+                return "```\n" + visitor.output + "\n```"
             except Exception as e:
-                return "ERROR: Uncaught exception: " + str(e)
+                # \u26A0 is the Unicode code (UTF-8) for the exclamation sign emoji (⚠️)
+                # Putting text *between asterisks* makes it bold
+                return "*\u26A0 ERROR: Uncaught exception: " + str(e) + " \u26A0*"
 
         if not update.effective_chat.id in context.bot_data:
             start(update, context)
@@ -31,12 +36,14 @@ def run():
 
         if ret != "":
             # If the execution generated any output, send it to the user
-            context.bot.send_message(chat_id=update.effective_chat.id, text=ret)
+            context.bot.send_message(chat_id=update.effective_chat.id, text=ret, parse_mode=ParseMode.MARKDOWN_V2)
 
     def img_callback(update, context, img):
-        """Image callback. Recieves a path and sends that image through Telegram"""
-        img.save("telegram-bot.png")  # Override the filename for security reasons.
-        context.bot.send_photo(chat_id=update.effective_chat.id, photo=open("telegram-bot.png", 'rb'), caption=img.filename)
+        """Image callback. Recieves the image, saves it to memory and sends it through Telegram"""
+        with io.BytesIO() as output:
+            img.save(output, format="PNG")
+            output.seek(0)
+            context.bot.send_photo(chat_id=update.effective_chat.id, photo=output, caption=img.filename)
 
     def start(update, context):
         """Initializes the bot for the given user"""
@@ -44,16 +51,10 @@ def run():
             del context.bot_data[update.effective_chat.id]
             context.bot.send_message(chat_id=update.effective_chat.id, text="Environment cleared")
 
-        # Create a new TreeVisitor class instance with a custom callback for images. This handler will override
-        # the path where the image will be saved to "telegram-bot.png" independently of what the user specified.
-        # This is mainly done for two reasons:
-        # 1) To prevent filling up my hard disk with random images
-        # 2) For security reasons. This way, we are safe against mallicious `draw` commands like the following:
-        #
-        #   draw "C:\Windows\System32\kernel32.dll", [0 0 1 1]
-        #
-        # This command would normally overwrite a system file, but because we override its name, it will be
-        # saved to "telegram-bot.png" regardless of the path specified.
+        # Create a new TreeVisitor class instance with a custom callback for images. This handler will intercept
+        # the image and, instead of saving it to disk like normally, it will save it to memory and send it through
+        # Telegram, completely ignoring the path specified. This is both faster and safer, since there is no way
+        # to create/overwrite files on the bot's PC using mallicious `draw` commands.
         context.bot_data[update.effective_chat.id] = TreeVisitor(lambda p: img_callback(update, context, p))
 
     # Read the private token
